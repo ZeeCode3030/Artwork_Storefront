b109df8906c434f6e7cfad607ba394ae896fe613
Hephzibah Nick-Oshomah <zibahnicks@gmail.com>
2023-06-19 20:34:06 -0400

    hopefully final commit before cyclic

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 /*********************************************************************************
- * WEB322 – Assignment 3
+ * WEB322 – Assignment 4
  * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
  * No part of this assignment has been copied manually or electronically from any other source
  * (including web sites) or distributed to other students.
  *
  * Name: Hephzibah Nick-Oshomah
  * Student ID: 180580219
- * Date: 06/06/2023
+ * Date: 19/06/2023
  *
  ********************************************************************************/
 var HTTP_PORT = process.env.PORT || 8080;
